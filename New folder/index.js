a85a4c44121490b5b5a8e66bfe4f3cb5179d7343
akadaica4 <82006138+akadaica4@users.noreply.github.com>
2021-12-13 18:08:59 +0700

    Rotoccho

---
+++ New folder/index.js
@@ -0,0 +1,7 @@
+function click() {
+    let ckick1 = document.getElementsByClassName('btn').value;
+    let input = document.getElementById('Result').value;
+    if (ckick1 != 'c') {
+        input += ckick1;
+    }
+}
